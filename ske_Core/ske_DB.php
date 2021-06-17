6a60bde92d9f00fc07b3724da843bded72ba11a2
kwangeun1991 <aosjtnstn7@naver.com>
2021-06-17 23:34:59 +0900

    .

--- ske_Core/ske_DB.php
+++ ske_Core/ske_DB.php
@@ -5,9 +5,9 @@ class ske_DB extends PDO
   public function __construct()
   {
     try {
-      $dsn = "mysql:host=localhost;dbname=kwang4585";
-      $username = "kwang4585";
-      $password = "ske201301";
+      $dsn = "mysql:host=localhost;dbname=111111";
+      $username = "111111";
+      $password = "111111";
 
       parent::__construct($dsn, $username, $password);
 
